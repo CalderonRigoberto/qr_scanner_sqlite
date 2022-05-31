810d05490cf40b31a716b3c8034aa93eea0a3ee4
Rigoberto Calderon <jesuscalflo15@gmail.com>
2022-05-30 23:16:06 -0500

    Implementando función de lectura de QR.

--- lib/widgets/custom_floating_button.dart
+++ lib/widgets/custom_floating_button.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class CustomFloatingButton extends StatelessWidget {
-  const CustomFloatingButton({ Key? key }) : super(key: key);
+  const CustomFloatingButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
       elevation: 0,
       child: const Icon(Icons.qr_code),
-      onPressed: (){},
+      onPressed: () async {
+        String barcodeValueScan;
+        try{
+          barcodeValueScan = await FlutterBarcodeScanner.scanBarcode(
+                                                    '#3D8BEF', 
+                                                    'Cancelar', 
+                                                    false, 
+                                                    ScanMode.QR);
+        }on PlatformException {
+          barcodeValueScan = 'Failed to get platform version.';
+        }
+
+
+      },
     );
   }
 }
