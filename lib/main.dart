e3c5ca0ee6db9811f54b305165aa1e3681eb2533
Rigoberto Calderon <jesuscalflo15@gmail.com>
2022-06-02 02:58:22 -0500

    Agregando rutas,

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:qr_scanner_sqlite/pages/maps_page.dart';
 import 'package:qr_scanner_sqlite/pages/pages.dart';
 import 'package:qr_scanner_sqlite/providers/option_provider.dart';
+import 'package:qr_scanner_sqlite/providers/scan_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,14 +17,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MultiProvider(
       providers:  [
-        ChangeNotifierProvider(create: ((context) =>  OptionSelected()))
+        ChangeNotifierProvider(create: ((context) =>  OptionSelected())),
+        ChangeNotifierProvider(create: ((context) =>  ScanProvider())),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         initialRoute: 'home_screen',
         routes: {
           'home_screen' : (context) => const HomePage(),
-          'maps'        : (context) => const MapPage()
+          'maps'        : (context) => const MapsPage(),
+          'map'         : (context) => const MapPage()
         },
         theme: ThemeData(
           primarySwatch: Colors.blueGrey,
